c56c61acb45d35580735a3afb0c5da8e61b7d56e
Vallegalan <vallegalan810@gmail.com>
2022-02-13 00:14:01 -0300

    instalando flex-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
+
 import { PianoComponent } from './componentes/piano/piano.component';
 import { DadosComponent } from './componentes/dados/dados.component';
 import { InicioComponent } from './componentes/inicio/inicio.component';
 import { DragonbzModule } from './dragonBz/dragonbz.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//import { MainPageComponent } from './dragonBz/main-page/main-page.component';
-import { MaterialModule } from './material/material.module';
- 
 
 
 @NgModule({
@@ -24,7 +24,7 @@ import { MaterialModule } from './material/material.module';
     BrowserModule,
     DragonbzModule,
     AppRoutingModule,
-
+    FlexLayoutModule,
     MaterialModule,
     BrowserAnimationsModule
   ],
