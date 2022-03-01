2e0b84cce6df8ce25fc4c2dfd886dc836e9f3107
Vallegalan <vallegalan810@gmail.com>
2022-02-28 23:22:13 -0300

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { pianoComponent } from './components/inicio/piano/piano.component';
- 
+import { DadosComponent } from './components/inicio/dados/dados.component';
+
 const routes: Routes = [
   
   {path:'',redirectTo:"/inicio",pathMatch:'full'},//si esta vacio voy a inicio
   
   {path:'inicio',component:InicioComponent,children:[
     {path: '',component: InicioComponent },
-    {path:'piano',component:pianoComponent},
+    
    
   ]},
+  {path:'piano',component:pianoComponent},
+    {path:'dados',component: DadosComponent},
 ];
 
 @NgModule({
